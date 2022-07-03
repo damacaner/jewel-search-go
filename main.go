87c5f8f6830dc12fbd31c72fd5a4afb0914d9104
Caner Çetin <92731060+damacaner@users.noreply.github.com>
2022-07-03 20:19:09 +0300

    behind, behind
    already descending

--- main.go
+++ main.go
@@ -12,178 +12,98 @@ import (
 )
 
 func main() {
-	timenow := time.Now()
 	var notable string
+	var notablecount int
 	var mapping = make(map[string]int)
-	var decoder = make(map[int]string)
-	// Take user input for notableinput variable
-	fmt.Println("Enter a notable:")
-	linescanner := bufio.NewScanner(os.Stdin)
-	linescanner.Scan()
-	line := linescanner.Text()
+	var notableamount []string
+	var decoder = make(map[string]int)
+	// Ask user to enter a desired notable amount to search and store every notable in a different value
+	fmt.Println("Enter a notable amount to search:")
+	fmt.Println("Minimum 4 is recommended, as search is not showing anything lower than 3.")
+	fmt.Scanln(&notablecount)
+	if notablecount > 8 {
+		fmt.Println("Please enter a number between 3 and 8.")
+		os.Exit(1)
+	}
+	for i := 0; i < notablecount; i++ {
+		// Take an user oinput for notable
+		fmt.Printf("Enter notable %d> ", i)
+		// Scan notable with buffer
+		scanner := bufio.NewScanner(os.Stdin)
+		scanner.Scan()
+		notable = scanner.Text()
+		// Append notable to array
+		notableamount = append(notableamount, notable)
+	}
 	notablefile, err := os.Open("notables.txt")
-	// Scan one more inputs from user for notableinput2 variable
-	fmt.Println("Enter another notable:")
-	linescanner.Scan()
-	line2 := linescanner.Text()
-	// Scan one more inputs from user for notableinput3 variable
-	fmt.Println("Enter another notable:")
-	linescanner.Scan()
-	line3 := linescanner.Text()
+	fmt.Println("Enter desired lethal pride seed:")
+	timenow := time.Now()
+	decodefile, err := os.Open("decode.txt")
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println("Enter desired lethal pride seed:")
 	lethal := bufio.NewScanner(os.Stdin)
 	lethal.Scan()
 	lethalpridemod := lethal.Text()
-	// Scan the lines of notablefile
-	scanner := bufio.NewScanner(notablefile)
-	for scanner.Scan() {
-		notable = scanner.Text()                  // Scan the words of notables
-		reg, err := regexp.Compile("[^a-zA-Z ]+") // Trim non-alphanumeric characters
+	var decodeCheck string
+	scannerdecode := bufio.NewScanner(decodefile)
+	for scannerdecode.Scan() {
+		decode := scannerdecode.Text()
+		reg, err := regexp.Compile("[^a-zA-Z]+")
 		if err != nil {
 			log.Fatal(err)
 		}
-		notableCheck := reg.ReplaceAllString(notable, "")
-		if notableCheck == line { // If trimmed string is equal to user input
-			reg, err := regexp.Compile("[^0-9]+")
-			if err != nil {
-				log.Fatal(err)
-			}
-			processedString := reg.ReplaceAllString(notable, "")
-			processedStringInteger, err := strconv.Atoi(processedString)
-			// Search the processedString in the Lethal pride seeds.csv file
-			seedfile, err := os.Open("Lethal pride seeds.csv")
-			if err != nil {
-				panic(err)
-			}
-			csv_reader := csv.NewReader(seedfile)
-			data, err := csv_reader.ReadAll()
-			if err != nil {
-				panic(err)
-			}
-			// Open decode.txt file
-			decodefile, err := os.Open("decode.txt")
-			if err != nil {
-				panic(err)
-			}
-			scanner := bufio.NewScanner(decodefile)
-			for scanner.Scan() {
-				decode := scanner.Text()
-				reg, err := regexp.Compile("[^a-zA-Z]+")
-				if err != nil {
-					log.Fatal(err)
-				}
-				decodeCheck := reg.ReplaceAllString(decode, "")
-				regnum2, err := regexp.Compile("[^0-9 ]+")
-				numbers := regnum2.ReplaceAllString(decode, "")
-				numbers_integer, _ := strconv.Atoi(numbers)
-				decoder[numbers_integer] = decodeCheck
-			}
-			for _, row := range data {
-				// Match row[processedString] to the decode.txt file
-				replacednode := row[processedStringInteger]
-				replaced_reg_int, _ := strconv.Atoi(replacednode)
-				if decoder[replaced_reg_int] == lethalpridemod {
-					if mapping[row[1]] == 0 {
-						mapping[row[1]] = 1
-					}
-					mapping[row[1]]++
-				}
-			}
+		decodeCheck = reg.ReplaceAllString(decode, "")
+		// Check if decodeCheck is equal to lethalpridemod
+		if decodeCheck == lethalpridemod {
+			fmt.Println("Decode found!")
+			fmt.Println("Decode:", decode)
+			regnum2, _ := regexp.Compile("[^0-9 ]+")
+			numbers := regnum2.ReplaceAllString(decode, "")
+			numbers_integer, _ := strconv.Atoi(numbers)
+			decoder[decodeCheck] = numbers_integer
+			break
 		}
-		if notableCheck == line2 { // If trimmed string is equal to user input
-			reg, err := regexp.Compile("[^0-9]+")
+	}
+	// Scan the lines of notablefile
+	scanner := bufio.NewScanner(notablefile)
+	for scanner.Scan() {
+		for line := range notableamount {
+			notable = scanner.Text()                  // Scan the words of notables
+			reg, err := regexp.Compile("[^a-zA-Z ]+") // Trim non-alphanumeric characters
 			if err != nil {
 				log.Fatal(err)
 			}
-			processedString := reg.ReplaceAllString(line2, "")
-			processedStringInteger, err := strconv.Atoi(processedString)
-			// Search the processedString in the Lethal pride seeds.csv file
-			seedfile, err := os.Open("Lethal pride seeds.csv")
-			if err != nil {
-				panic(err)
-			}
-			csv_reader := csv.NewReader(seedfile)
-			data, err := csv_reader.ReadAll()
-			if err != nil {
-				panic(err)
-			}
-			// Open decode.txt file
-			decodefile, err := os.Open("decode.txt")
-			if err != nil {
-				panic(err)
-			}
-			scanner := bufio.NewScanner(decodefile)
-			for scanner.Scan() {
-				decode := scanner.Text()
-				reg, err := regexp.Compile("[^a-zA-Z]+")
+			notableCheck := reg.ReplaceAllString(notable, "")
+			if notableCheck == notableamount[line] { // If trimmed string is equal to user input
+				reg, err := regexp.Compile("[^0-9]+")
 				if err != nil {
 					log.Fatal(err)
 				}
-				decodeCheck := reg.ReplaceAllString(decode, "")
-				regnum2, err := regexp.Compile("[^0-9 ]+")
-				numbers := regnum2.ReplaceAllString(decode, "")
-				numbers_integer, _ := strconv.Atoi(numbers)
-				decoder[numbers_integer] = decodeCheck
-			}
-			for _, row := range data {
-				// Match row[processedString] to the decode.txt file
-				replacednode := row[processedStringInteger]
-				replaced_reg_int, _ := strconv.Atoi(replacednode)
-				if decoder[replaced_reg_int] == lethalpridemod {
-					if mapping[row[1]] == 0 {
-						mapping[row[1]] = 1
-					}
-					mapping[row[1]]++
-				}
-			}
-		}
-		if notableCheck == line3 { // If trimmed string is equal to user input
-			reg, err := regexp.Compile("[^0-9]+")
-			if err != nil {
-				log.Fatal(err)
-			}
-			processedString := reg.ReplaceAllString(line3, "")
-			processedStringInteger, err := strconv.Atoi(processedString)
-			// Search the processedString in the Lethal pride seeds.csv file
-			seedfile, err := os.Open("Lethal pride seeds.csv")
-			if err != nil {
-				panic(err)
-			}
-			csv_reader := csv.NewReader(seedfile)
-			data, err := csv_reader.ReadAll()
-			if err != nil {
-				panic(err)
-			}
-			// Open decode.txt file
-			decodefile, err := os.Open("decode.txt")
-			if err != nil {
-				panic(err)
-			}
-			scanner := bufio.NewScanner(decodefile)
-			for scanner.Scan() {
-				decode := scanner.Text()
-				reg, err := regexp.Compile("[^a-zA-Z]+")
+				processedString := reg.ReplaceAllString(notable, "")
+				processedStringInteger, err := strconv.Atoi(processedString)
+				// Search the processedString in the Lethal pride seeds.csv file
+				seedfile, err := os.Open("Lethal pride seeds.csv")
 				if err != nil {
-					log.Fatal(err)
+					panic(err)
 				}
-				decodeCheck := reg.ReplaceAllString(decode, "")
-				regnum2, err := regexp.Compile("[^0-9 ]+")
-				numbers := regnum2.ReplaceAllString(decode, "")
-				numbers_integer, _ := strconv.Atoi(numbers)
-				decoder[numbers_integer] = decodeCheck
-			}
-			for _, row := range data {
-				// Match row[processedString] to the decode.txt file
-				replacednode := row[processedStringInteger]
-				replaced_reg_int, _ := strconv.Atoi(replacednode)
-				if decoder[replaced_reg_int] == lethalpridemod {
-					if mapping[row[1]] == 0 {
-						mapping[row[1]] = 1
+				csv_reader := csv.NewReader(seedfile)
+				data, err := csv_reader.ReadAll()
+				if err != nil {
+					panic(err)
+				}
+				for _, row := range data {
+					// Match row[processedString] to the decode.txt file
+					replacednode := row[processedStringInteger]
+					replaced_reg_int, _ := strconv.Atoi(replacednode)
+					fmt.Println(decoder[decodeCheck], replaced_reg_int)
+					if decoder[decodeCheck] == replaced_reg_int {
+						if mapping[row[1]] == 0 {
+							mapping[row[1]] = 1
+						}
+						mapping[row[1]] = mapping[row[1]] + 1
+						continue
 					}
-					mapping[row[1]]++
 				}
 			}
 		}
@@ -195,10 +115,14 @@ func main() {
 	csv_reader := csv.NewReader(seedfile)
 	data, err := csv_reader.ReadAll()
 	fmt.Println("Only showing the seeds that higher than 3 occurences, if you dont see anything, then it doesnt exist.")
+	// Create the seeds.csv file
+	file, err := os.Create("seeds.csv")
 	for _, row := range data {
 		for i := range mapping {
 			if mapping[i] >= 3 {
 				fmt.Println("Seed>", row[1])
+				// Append row[1] to the seeds.csv file
+				file.WriteString(row[1] + "\n")
 			}
 		}
 	}
